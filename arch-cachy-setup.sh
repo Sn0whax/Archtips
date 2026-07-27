5386031527f0f8f69d60b915f9a329f1a1a9825d
Sn0whax <12345678+Sn0whax@users.noreply.github.com>
2026-07-26 22:24:47 -0500

    Add Nano

--- arch-cachy-setup.sh
+++ arch-cachy-setup.sh
@@ -50,7 +50,7 @@ PLASMA_PACKAGES=(
 )
 KERNEL_PACKAGES=(cachyos-settings cachyos-hello linux-cachyos linux-cachyos-headers proton-cachyos-slr wine-cachyos)
 USER_PACKAGES=(
-  systemd-boot-manager python-pynvml fish alsa-utils btop cava xorg-xwininfo
+  systemd-boot-manager python-pynvml fish nano alsa-utils btop cava xorg-xwininfo
   steam okular kcalc protonup-qt fastfetch ttf-migu ttf-hack-nerd ttf-baekmuk obs-studio brave-origin-bin
   discord mpv audacious haruna signal-desktop lact faugus-launcher
   phonon-qt6-mpv-git yt-dlp apparmor cifs-utils ufw
